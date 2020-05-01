c70f643e8f31358eaf64e8d48fabe66a8358dfca
Anna Gladzińska <agladzinska@gmail.com>
2020-05-01 10:31:59 +0200

    App publication

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +13,11 @@ app.use(express.urlencoded({
     extended: false
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
@@ -21,6 +27,6 @@ app.use((req, res) => {
     res.status(404).send('404 not found...');
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
 });
